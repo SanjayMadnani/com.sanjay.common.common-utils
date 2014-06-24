ecc2637f104268dbfad3b990f3bbea1a32d041f6
Sanjay Madnani <sanjay.madnani@outlook.com>
2014-06-24 23:55:19 +0530

    Initial Commit: Simple structure.
    Signed-off-by: Sanjay Madnani <sanjay.madnani@outlook.com>

---
+++ common-utils/src/main/java/com/sanjay/common/exception/ApplicationException.java
@@ -0,0 +1,18 @@
+/* Copyright (C) 2014, 2015 Sanjay Madnani
+ * 
+ * This file is free to use: you can redistribute it and/or modify it under the terms of the GPL General Public License
+ * V2 as published by the Free Software Foundation, subject to the following conditions:
+ * 
+ * The above copyright notice should never be changed and should always included wherever this file is used.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License V2 for more details. */
+package com.sanjay.common.exception;
+
+/**
+ * @author SANJAY
+ * 
+ */
+public class ApplicationException extends Exception {
+
+}
---
+++ common-utils/src/test/java/com/sanjay/common/util/FileUtilTest.java
@@ -0,0 +1,43 @@
+/* Copyright (C) 2014, 2015 Sanjay Madnani
+ * 
+ * This file is free to use: you can redistribute it and/or modify it under the terms of the GPL General Public License
+ * V2 as published by the Free Software Foundation, subject to the following conditions:
+ * 
+ * The above copyright notice should never be changed and should always included wherever this file is used.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License V2 for more details. */
+package com.sanjay.common.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author SANJAY
+ * 
+ */
+public class FileUtilTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public final void test() {
+        fail("Not yet implemented");
+    }
+
+}
